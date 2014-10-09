08962346b2f86b474922708c46ba8e6a7f59bd0c
elina <elinamuhhamedjanova@gmail.com>
2014-10-09 20:00:10 +0300

    delete some not used activities

--- gen/activ/foody/R.java
+++ gen/activ/foody/R.java
@@ -12,23 +12,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int back=0x7f020000;
-        public static final int cuibutton=0x7f020001;
-        public static final int cuifooter=0x7f020002;
-        public static final int cuisines=0x7f020003;
-        public static final int french=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int italian=0x7f020006;
-        public static final int japan=0x7f020007;
-        public static final int medieval=0x7f020008;
-        public static final int menulogo=0x7f020009;
-        public static final int pizza=0x7f02000a;
-        public static final int restaurants=0x7f02000b;
-        public static final int restbutton=0x7f02000c;
-        public static final int restfooter=0x7f02000d;
-        public static final int russian=0x7f02000e;
-        public static final int searchicon=0x7f02000f;
-        public static final int welcome=0x7f020010;
-        public static final int welcomescreen=0x7f020011;
+        public static final int cui=0x7f020001;
+        public static final int cuibutton=0x7f020002;
+        public static final int cuifooter=0x7f020003;
+        public static final int cuisines=0x7f020004;
+        public static final int footer=0x7f020005;
+        public static final int french=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int italian=0x7f020008;
+        public static final int japan=0x7f020009;
+        public static final int logo=0x7f02000a;
+        public static final int medieval=0x7f02000b;
+        public static final int menu=0x7f02000c;
+        public static final int menu5=0x7f02000d;
+        public static final int menulogo=0x7f02000e;
+        public static final int pizza=0x7f02000f;
+        public static final int rest=0x7f020010;
+        public static final int restaurants=0x7f020011;
+        public static final int restbutton=0x7f020012;
+        public static final int restfooter=0x7f020013;
+        public static final int russian=0x7f020014;
+        public static final int search=0x7f020015;
+        public static final int searchicon=0x7f020016;
+        public static final int welcome=0x7f020017;
+        public static final int welcomescreen=0x7f020018;
     }
     public static final class id {
         public static final int CuisinesButton=0x7f06000a;
@@ -42,11 +49,12 @@ public final class R {
         public static final int imageView1=0x7f060004;
         public static final int imageView2=0x7f060005;
         public static final int imageView3=0x7f06000c;
-        public static final int inputDesc=0x7f06000f;
-        public static final int inputName=0x7f06000d;
-        public static final int inputPrice=0x7f06000e;
+        public static final int inputDesc=0x7f060010;
+        public static final int inputName=0x7f06000e;
+        public static final int inputPrice=0x7f06000f;
         public static final int name=0x7f060009;
         public static final int rest_id=0x7f060008;
+        public static final int textView1=0x7f06000d;
     }
     public static final class layout {
         public static final int all_restaurants=0x7f030000;
@@ -56,8 +64,10 @@ public final class R {
         public static final int cuispage3=0x7f030004;
         public static final int list_item=0x7f030005;
         public static final int menu=0x7f030006;
-        public static final int restinfo=0x7f030007;
-        public static final int welcome=0x7f030008;
+        public static final int rest=0x7f030007;
+        public static final int restinfo=0x7f030008;
+        public static final int search=0x7f030009;
+        public static final int welcome=0x7f03000a;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
